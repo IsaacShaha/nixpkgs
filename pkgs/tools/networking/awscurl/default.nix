846f569fa60a48c7b8488319e9546807df875941
Isaac Shaha <isaac.shaha64@gmail.com>
2023-07-15 13:14:07 -0700

    Tests passing, awscurl working and python not in path. Just need to make sure awscurl works properly now.

--- pkgs/tools/networking/awscurl/default.nix
+++ pkgs/tools/networking/awscurl/default.nix
@@ -1,50 +1,64 @@
 { fetchFromGitHub
 , lib
+, makeWrapper
 , python3
-}:
+}: with python3.pkgs;
+let
+  awscurl = buildPythonApplication rec {
+    pname = "awscurl";
+    version = "0.29";
+    format = "setuptools";
 
-with python3.pkgs;
+    src = fetchFromGitHub {
+      owner = "okigan";
+      repo = "awscurl";
+      rev = "e47119b";
+      hash = "sha256-NVGPB29kgLYpJnAiSUYoC9M11G9J1shbe1ZXbywRyUU=";
+    };
 
-buildPythonApplication rec {
-  pname = "awscurl";
-  version = "0.29";
-  format = "setuptools";
+    doCheck = true;
 
-  src = fetchFromGitHub {
-    owner = "okigan";
-    repo = "awscurl";
-    rev = "e47119b";
-    hash = "sha256-NVGPB29kgLYpJnAiSUYoC9M11G9J1shbe1ZXbywRyUU=";
+    checkInputs = [
+      mock
+      pytest
+    ];
+
+    # Remove tests that require access to the internet or home directory.
+    checkPhase = ''
+      python -m pytest -k 'not integration_test'
+    '';
+
+    buildInputs = [
+      cryptography
+      pyopenssl
+    ];
+
+    propagatedBuildInputs = [
+      botocore
+      configargparse
+      configparser
+      requests
+      urllib3
+    ];
+
+    meta = {
+      description = "curl-like tool with AWS Signature Version 4 request signing.";
+      homepage = "https://github.com/okigan/awscurl";
+      license = lib.licenses.mit;
+      maintainers = with maintainers; [ isaacshaha ];
+    };
   };
+in
+stdenv.mkDerivation rec {
+  inherit (awscurl) pname version meta;
 
-  doCheck = true;
-
-  checkInputs = [
-    mock
-    pytest
+  nativeBuildInputs = [
+    makeWrapper
   ];
 
-  checkPhase = ''
-    python -m pytest -k 'not request'
+  # Wrap awscurl so that Python is not in the system path.
+  buildCommand = ''
+    makeWrapper ${awscurl}/bin/awscurl $out/bin/awscurl \
+      --set PYTHONHOME "${python3}"
   '';
-
-  buildInputs = [
-    cryptography
-    pyopenssl
-  ];
-
-  propagatedBuildInputs = [
-    botocore
-    configargparse
-    configparser
-    requests
-    urllib3
-  ];
-
-  meta = with lib; {
-    description = "curl-like tool with AWS Signature Version 4 request signing.";
-    homepage = "https://github.com/okigan/awscurl";
-    license = licenses.mit;
-    maintainers = with maintainers; [ isaacshaha ];
-  };
 }
